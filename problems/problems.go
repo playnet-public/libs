bc2f406397b6b5f9a4c5586cdf20dfffbf51fd58
Vincent <agpheaddikilla@yahoo.de>
2018-11-13 09:53:09 +0100

    fix formal errors

--- problems/problems.go
+++ problems/problems.go
@@ -1,10 +1,13 @@
-package errors
+package problems
 
 import (
 	"fmt"
 )
 
-//Problem implements the RFC7807 "problem"/error standard
+//DefaultType implements the default type content described in RFC 7807
+const DefaultType = "about:blank"
+
+//Problem implements the RFC 7807 "problem"/error standard
 //Additional error field can be defined in custom struct implementing the Problem struct
 type Problem struct {
 	Title    string `json:"title"`
@@ -14,61 +17,51 @@ type Problem struct {
 	Status   int    `json:"status,omitempty"`
 }
 
-//Error implements the error interface
+//New creates a new Problem with all object details described in RFC 7807
+func New(title, detail string) *Problem {
+	return &Problem{
+		Title:  title,
+		Detail: detail,
+		Type:   DefaultType,
+	}
+}
+
+//Error returns the error of the problem
 func (p *Problem) Error() string {
 	return fmt.Sprintf("Title: %s; Detail: %s", p.Title, p.Detail)
 }
 
-//DefaultType implements the default type content described in RFC7807
-const DefaultType = "about:blank"
-
-//SetTitle sets the title of the problem object
+//SetTitle sets the title field of the problem, specified in RFC 7807
 func (p *Problem) SetTitle(title string) *Problem {
 	p.Title = title
 	return p
 }
 
-//SetDetail sets the detail info of the problem object
+//SetDetail sets the detail field of the problem, specified in RFC 7807
 func (p *Problem) SetDetail(detail string) *Problem {
 	p.Detail = detail
 	return p
 }
 
-//SetType sets the type of the problem object
-func (p *Problem) SetType(typ string) *Problem {
-	if typ == "" {
+//SetType sets the detail field of the problem, specified in RFC 7807
+func (p *Problem) SetType(t string) *Problem {
+	if t == "" {
 		p.Type = DefaultType
 	} else {
-		p.Type = typ
+		p.Type = t
 	}
 
 	return p
 }
 
-//SetInstance sets the instance of the problem object
+//SetInstance sets the instance field of the problem, specified in RFC 7807
 func (p *Problem) SetInstance(instance string) *Problem {
 	p.Instance = instance
 	return p
 }
 
-//SetStatus sets the HTTP status of the problem object HTTP request
+//SetStatus sets the status field of the problem, specified in RFC 7807
 func (p *Problem) SetStatus(status int) *Problem {
 	p.Status = status
 	return p
-}
-
-//New creates a new Problem with all object details described in RFC7807
-func New() *Problem {
-	return &Problem{
-		Type: DefaultType,
-	}
-}
-
-//NewFromError returns a new Problem object from an error interface
-//The error message is replaced with the detail field
-func NewFromError(err error) *Problem {
-	return &Problem{
-		Type:   DefaultType,
-		Detail: err.Error(),
-	}
 }
