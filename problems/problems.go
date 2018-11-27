995f231c1cdeef0385aeccf18165e5d457375a9f
Vincent Heins <agpheaddikilla@yahoo.de>
2018-11-27 22:28:41 +0100

    Update problems.go

--- problems/problems.go
+++ problems/problems.go
@@ -1,7 +1,7 @@
 package problems
 
 import (
-	"fmt"
+	"github.com/pkg/errors"
 )
 
 //DefaultType implements the default type content described in RFC 7807
@@ -15,20 +15,77 @@ type Problem struct {
 	Type     string `json:"type,omitempty"`
 	Instance string `json:"instance,omitempty"`
 	Status   int    `json:"status,omitempty"`
+	err      error
+}
+
+//Wrap is an alias to github.com/pkg/errors Wrap function. If Problem pointer passed as error, it sets the error and a new message of Problem.
+func Wrap(err error, msg string) error {
+	if err == nil {
+		return nil
+	}
+
+	problem, ok := err.(*Problem)
+	if !ok {
+		return errors.Wrap(err, msg)
+	}
+
+	problem.err = errors.Wrap(problem.err, msg)
+	return problem
+}
+
+//WithStack is an alias to github.com/pkg/errors WithStack function. If Problem pointer passed as error, it sets the error of Problem.
+func WithStack(err error) error {
+	if err == nil {
+		return nil
+	}
+
+	problem, ok := err.(*Problem)
+	if !ok {
+		return errors.WithStack(err)
+	}
+
+	problem.err = errors.WithStack(problem.err)
+	return problem
+}
+
+//WithMessage is an alias to github.com/pkg/errors WithMessage function. If Problem pointer passed as error, it sets the error, stacktrace and message of Problem.
+func WithMessage(err error, msg string) error {
+	if err == nil {
+		return nil
+	}
+
+	problem, ok := err.(*Problem)
+	if !ok {
+		return errors.WithMessage(err, msg)
+	}
+
+	problem.err = errors.WithMessage(problem.err, msg)
+	return problem
 }
 
 //New creates a new Problem with all object details described in RFC 7807
-func New(title, detail string) *Problem {
+func New(title, detail string, status int) error {
+	return newWithError(
+		errors.New(detail),
+		title,
+		detail,
+		status,
+	)
+}
+
+func newWithError(err error, title, detail string, status int) *Problem {
 	return &Problem{
-		Title:  title,
-		Detail: detail,
 		Type:   DefaultType,
+		Title:  title,
+		Detail: err.Error(),
+		Status: status,
+		err:    err,
 	}
 }
 
 //Error returns the error of the problem
 func (p Problem) Error() string {
-	return fmt.Sprintf("Title: %s; Detail: %s", p.Title, p.Detail)
+	return p.err.Error()
 }
 
 //SetTitle sets the title field of the problem, specified in RFC 7807
